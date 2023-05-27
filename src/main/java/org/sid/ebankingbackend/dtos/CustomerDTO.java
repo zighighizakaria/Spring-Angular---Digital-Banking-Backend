7003d48cd866f3ef0d90c41a60591f40997dee33
zighighizakaria <zighighi.zakaria.2000@gmail.com>
2023-05-27 15:48:13 +0100

    zighighi backend

---
+++ src/main/java/org/sid/ebankingbackend/dtos/CustomerDTO.java
@@ -0,0 +1,16 @@
+package org.sid.ebankingbackend.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.ebankingbackend.entities.BankAccount;
+
+import java.util.List;
+
+@Data
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String email;
+}
